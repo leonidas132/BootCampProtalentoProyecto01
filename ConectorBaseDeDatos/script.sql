d77c9b27a0caa8877cc63390abc3e8f0883f9936
Octavio Robleto <octavio.robleto@gmail.com>
2022-06-23 19:49:52 -0300

    Script base de datos Inicial

---
+++ ConectorBaseDeDatos/script.sql
@@ -0,0 +1,13 @@
+create database if not exists Protalento01;
+use Protalento01;
+
+create table if not exists usuarios(
+	correo varchar(100) not null primary key,
+	clave blob,-- poder encriptar la informacion 
+	fechaCreacion date not null
+); 
+
+create table if not exists categorias(
+	id bigint auto_increment primary key,
+	descripcion varchar(100)
+);
